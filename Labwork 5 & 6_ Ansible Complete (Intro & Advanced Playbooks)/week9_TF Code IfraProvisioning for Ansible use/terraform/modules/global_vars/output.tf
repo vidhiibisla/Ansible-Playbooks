e66521c80856fcbdf6e65f46959e8d7065309db7
vidhiibisla <110334040+vidhiibisla@users.noreply.github.com>
2025-01-11 22:06:07 -0500

    Add files via upload

---
+++ Labwork 5 & 6_ Ansible Complete (Intro & Advanced Playbooks)/week9_TF Code IfraProvisioning for Ansible use/terraform/modules/global_vars/output.tf
@@ -0,0 +1,17 @@
+output "default_tags" {
+  value = {
+
+    Environment = "Test"
+    Owner       = "ACS730"
+    Project     = "Ansible"
+
+  }
+}
+
+output "prefix" {
+  value ="week7"
+}
+
+output "env" {
+  value ="dev"
+}
